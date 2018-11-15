7480f89ab1e0d85e0230caaa744a2a1c1f84deee
Philipp Schafer <mail@philippschafer.ch>
2018-11-15 15:49:31 +0100

    initialize project with rollup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <form>
-          <table>
-            <tr>
-              <td><label>Name</label></td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td><label>Vorname</label></td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td><label>Email</label></td>
-              <td><input type="email" /></td>
-            </tr>
-            <tr>
-              <td><label>Telefon</label></td>
-              <td><input type="phone" /></td>
-            </tr>
-          </table>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default App = () => (
+  <div>
+    <form>
+      <table>
+        <tr>
+          <td><label>Name</label></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><label>Vorname</label></td>
+          <td><input type="text" /></td>
+        </tr>
+        <tr>
+          <td><label>Email</label></td>
+          <td><input type="email" /></td>
+        </tr>
+        <tr>
+          <td><label>Telefon</label></td>
+          <td><input type="phone" /></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+);
