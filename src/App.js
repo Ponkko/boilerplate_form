f07ecdf2783a3d51002ef76e94337089b2d47575
Philipp Schafer <mail@philippschafer.ch>
2018-11-15 13:06:22 +0100

    add some form fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <table>
+            <tr>
+              <td><label>Name</label></td>
+              <td><input type="text" /></td>
+            </tr>
+            <tr>
+              <td><label>Vorname</label></td>
+              <td><input type="text" /></td>
+            </tr>
+            <tr>
+              <td><label>Email</label></td>
+              <td><input type="email" /></td>
+            </tr>
+            <tr>
+              <td><label>Telefon</label></td>
+              <td><input type="phone" /></td>
+            </tr>
+          </table>
+        </form>
       </div>
     );
   }
